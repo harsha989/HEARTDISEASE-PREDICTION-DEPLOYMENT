c5d9a02bc86e405c0ab8b100cc18337fe82f5022
harsha989 <53350799+harsha989@users.noreply.github.com>
2020-06-14 11:25:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,25 +17,24 @@ def welcome():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-        age=int(request.form['age'])
-        sex=int(request.form['sex'])
-        cp=int(request.form['cp'])
-        restbp=float(request.form['restbp'])
-        chol=float(request.form['chol'])
-        fbs=float(request.form['fbs'])
-        restecg=int(request.form['restecg'])
-        thalach=float(request.form['thalach'])
-        exang=int(request.form['exang'])
-        oldpeak=float(request.form['oldpeak'])
-        slope=int(request.form['slope'])
-        ca=float(request.form['ca'])
-        thal=int(request.form['thal'])
+      age=int(request.form['age'])
+      sex=int(request.form['sex'])
+      cp=int(request.form['cp'])
+      restbp=float(request.form['restbp'])
+      chol=float(request.form['chol'])
+      fbs=float(request.form['fbs'])
+      restecg=int(request.form['restecg'])
+      thalach=float(request.form['thalach'])
+      exang=int(request.form['exang'])
+      oldpeak=float(request.form['oldpeak'])
+      slope=int(request.form['slope'])
+      ca=float(request.form['ca'])
+      thal=int(request.form['thal'])
 
-        data = np.array([[age,sex,cp,restbp,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
-        my_prediction=classifier.predict(data)
+      data = np.array([[age,sex,cp,restbp,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
+      my_prediction=classifier.predict(data)
 
-        return render_template('result.html', prediction=my_prediction)
+      return render_template('result.html', prediction=my_prediction)
 
 
 
